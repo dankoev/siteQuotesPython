cf3e72b792598bf7b8611875aef01be505159423
Anigirka <dek17@tpu.ru>
2023-03-15 11:22:51 +0700

    change pagination logic

--- services/Pagination.py
+++ services/Pagination.py
@@ -27,36 +27,41 @@ class Paginator:
 
     def get_page_items(self, current_page):
         sp_n = self.split_number
-        if current_page:
-            current_page = int(current_page)
-            if current_page == 1:
-                previous_page = None
-                if current_page == self.number_pages:
-                    next_page = None
-                else:
-                    next_page = 2
-            elif current_page == self.number_pages:
-                previous_page = self.number_pages - 1
-                next_page = None
-            else:
-                previous_page = current_page - 1
-                next_page = current_page + 1
-        else:
-            current_page = 1
-            previous_page = None
-            if current_page == self.number_pages:
-                next_page = None
-            else:
-                next_page = 2
+        paginator = self.get_paginator(current_page)
         if not self.model_filter:
-            page_objs = self.model.objects.all()[sp_n * (current_page - 1):sp_n * current_page]
+            page_objs = self.model.objects.all()[sp_n * (paginator['current'] - 1):sp_n * paginator['current']]
         else:
-            page_objs = self.model.objects.filter(keys__keyword=self.model_filter)[sp_n * (current_page - 1):sp_n * current_page]
+            page_objs = self.model.objects.filter(keys__keyword=self.model_filter)[
+                        sp_n * (paginator['current'] - 1):sp_n * paginator['current']]
         items = {
             'pages': range(1, self.number_pages + 1),
             'page_objs': page_objs,
-            'current_page': current_page,
-            'next_page': next_page,
-            'previous_page': previous_page,
+            'current_page': paginator['current'],
+            'next_page': paginator['next'],
+            'previous_page': paginator['previous'],
         }
         return items
+
+    def get_paginator(self, current_page):
+
+        paginator = {
+            'current': None,
+            'next': None,
+            'previous': None,
+        }
+        print(current_page)
+        if not current_page or current_page == '1':
+            paginator['current'] = 1
+            if current_page != self.number_pages:
+                paginator['next'] = 2
+            return paginator
+
+        current_page = int(current_page)
+        paginator['current'] = current_page
+        if current_page == self.number_pages:
+            paginator['previous'] = self.number_pages - 1
+            return paginator
+
+        paginator['previous'] = current_page - 1
+        paginator['next'] = current_page + 1
+        return paginator
